a91246b85023712f76d65e3d9eb905ab60fab98b
JustAGuest <rich.cat.0001@gmail.com>
2026-05-13 13:10:36 +0300

    Update Init.lua

--- Game-specific/CombatGUI/Init.lua
+++ Game-specific/CombatGUI/Init.lua
@@ -2,12 +2,26 @@ local function load(URL)
     return loadstring(game:HttpGet(URL))()
 end
 
-local ok = pcall(function()
-    local Disclaimer = load("https://raw.githubusercontent.com/JustSomeGuest/Scripts/main/Disclaimer.lua")
-    Disclaimer.Script = "https://raw.githubusercontent.com/JustSomeGuest/Scripts/main/Game-specific/CombatGUI/Script.lua"
-    Disclaimer:Start()
-end)
+local WindUI = load("https://raw.githubusercontent.com/Footagesus/WindUI/main/dist/main.lua")
 
-if not ok then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/JustSomeGuest/Scripts/main/Game-specific/CombatGUI/Script.lua"))()
+local function Notify(text)
+    WindUI:Notify({
+        Title = "CombatGUI+",
+        Content = text,
+        Duration = 3
+    })
+end
+
+if game.GameId == 3808081382 then
+    local ok = pcall(function()
+        local Disclaimer = load("https://raw.githubusercontent.com/JustSomeGuest/Scripts/main/Disclaimer.lua")
+        Disclaimer.Script = "https://raw.githubusercontent.com/JustSomeGuest/Scripts/main/Game-specific/CombatGUI/Script.lua"
+        Disclaimer:Start()
+    end)
+
+    if not ok then
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/JustSomeGuest/Scripts/main/Game-specific/CombatGUI/Script.lua"))()
+    end
+else
+    Notify("CombatGUI+ only supports The Strongest Battlegrounds")
 end
