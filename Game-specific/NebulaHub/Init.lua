9f5fdf72b39c14a5542ba74f92647242fe0b1dac
JustAGuest <rich.cat.0001@gmail.com>
2026-05-13 13:11:10 +0300

    Update Init.lua

--- Game-specific/NebulaHub/Init.lua
+++ Game-specific/NebulaHub/Init.lua
@@ -2,26 +2,22 @@ local function load(URL)
     return loadstring(game:HttpGet(URL))()
 end
 
-local WindUI = load("https://raw.githubusercontent.com/Footagesus/WindUI/main/dist/main.lua")
+if game.GameId == 3808081382 then
+    local ok = pcall(function()
+        local Disclaimer = load("https://raw.githubusercontent.com/JustSomeGuest/Scripts/main/Disclaimer.lua")
+        Disclaimer.Script = "https://raw.githubusercontent.com/JustSomeGuest/Scripts/main/Game-specific/NebulaHub/Script.lua"
+        Disclaimer:Start()
+    end)
 
-local function Notify(text)
+    if not ok then
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/JustSomeGuest/Scripts/main/Game-specific/NebulaHub/Script.lua"))()
+    end
+else
+    local WindUI = load("https://raw.githubusercontent.com/Footagesus/WindUI/main/dist/main.lua")
+
     WindUI:Notify({
         Title = "Nebula Hub",
-        Content = text,
+        Content = "Nebula Hub only supports The Strongest Battlegrounds",
         Duration = 3
     })
-end
-
-if game.GameId ~= 3808081382 then
-    return Notify("Nebula Hub only supports The Strongest Battlegrounds")
-end
-
-local ok = pcall(function()
-    local Disclaimer = load("https://raw.githubusercontent.com/JustSomeGuest/Scripts/main/Disclaimer.lua")
-    Disclaimer.Script = "https://raw.githubusercontent.com/JustSomeGuest/Scripts/main/Game-specific/NebulaHub/Script.lua"
-    Disclaimer:Start()
-end)
-
-if not ok then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/JustSomeGuest/Scripts/main/Game-specific/NebulaHub/Script.lua"))()
 end
